654eb527be3a07f94d22a1189d530e5dc77f1d7d
YudhaAP <muhyudhapratama55@gmail.com>
2025-03-03 10:44:57 +0700

    feat(home): Create layout home

--- app/src/main/java/dev/mayutama/project/eventapp/ui/main/MainActivity.kt
+++ app/src/main/java/dev/mayutama/project/eventapp/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package dev.mayutama.project.eventapp.ui.main
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -9,9 +11,12 @@ import androidx.navigation.ui.setupWithNavController
 import dev.mayutama.project.eventapp.R
 import dev.mayutama.project.eventapp.base.BaseActivity
 import dev.mayutama.project.eventapp.databinding.ActivityMainBinding
+import dev.mayutama.project.eventapp.util.hide
+import dev.mayutama.project.eventapp.util.show
 
 class MainActivity :
-    BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate)
+    BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate),
+    OnClickListener
 {
     private val mainViewModel: MainViewModel by viewModels(){ MainViewModelFactory.getInstance(this.application) }
 
@@ -20,22 +25,57 @@ class MainActivity :
         super.onCreate(savedInstanceState)
 
         init()
+        initListener()
     }
 
     private fun init(){
+        setSupportActionBar(binding.appBar)
         setupBottomNav()
+
+        binding.searchView
+            .editText
+            .setOnEditorActionListener { textView, i, keyEvent ->
+                binding.searchView.hide()
+
+                false
+            }
+    }
+
+    private fun initListener(){
+        binding.imgSearch.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.img_search -> onClickSearch()
+        }
+    }
+
+    fun onClickSearch(){
+        binding.searchView.show()
     }
 
     private fun setupBottomNav(){
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navController.addOnDestinationChangedListener{_, destination, _ ->
+            binding.imgSearch.show()
+            binding.imgNotification.show()
+            binding.tvCountNotif.show()
             when(destination.id) {
-                R.id.navigation_home -> setTitleAppBar(getString(R.string.title_home))
+                R.id.navigation_home -> {
+                    binding.imgSearch.hide()
+                    setTitleAppBar(getString(R.string.title_home))
+                }
                 R.id.navigation_upcoming -> setTitleAppBar(getString(R.string.upcoming))
                 R.id.navigation_finished -> setTitleAppBar(getString(R.string.finished))
                 R.id.navigation_favorite -> setTitleAppBar(getString(R.string.favorite))
-                R.id.navigation_setting -> setTitleAppBar(getString(R.string.setting))
+                R.id.navigation_setting -> {
+                    binding.imgSearch.hide()
+                    binding.imgNotification.hide()
+                    binding.tvCountNotif.hide()
+                    setTitleAppBar(getString(R.string.setting))
+                }
             }
         }
         navView.setupWithNavController(navController)
--- app/src/main/java/dev/mayutama/project/eventapp/ui/main/home/HomeFragment.kt
+++ app/src/main/java/dev/mayutama/project/eventapp/ui/main/home/HomeFragment.kt
@@ -1,23 +1,55 @@
 package dev.mayutama.project.eventapp.ui.main.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.TextView
+import android.view.View.OnClickListener
 import androidx.fragment.app.viewModels
+import com.google.android.material.search.SearchView
+import dev.mayutama.project.eventapp.R
 import dev.mayutama.project.eventapp.base.BaseFragment
 import dev.mayutama.project.eventapp.databinding.FragmentHomeBinding
+import dev.mayutama.project.eventapp.ui.main.MainActivity
 
 class HomeFragment :
-    BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate)
+    BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate),
+    OnClickListener
 {
+    private lateinit var searchView: SearchView
+
     private val homeViewModel: HomeViewModel by viewModels()
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        initProperties(view)
+        init()
+        initListener()
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.search_bar -> onClickSearchBar()
         }
+    }
+
+    private fun init() {
+    }
+
+    private fun initProperties(view: View) {
+    }
+
+    private fun initListener() {
+        binding.searchBar.setOnClickListener(this)
+    }
+
+    private fun onClickSearchBar() {
+        (requireActivity() as MainActivity).onClickSearch()
     }
 }
